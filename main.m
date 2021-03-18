f765e376dbc83c4951405cc930f90e77aafb65fb
vini-cc <vinicius.conti.costa@gmail.com>
2021-03-18 20:33:39 -0300

    Creation of explore_Lidar

--- main.m
+++ main.m
@@ -5,13 +5,18 @@ clc;                % clean terminal
 profile clear       % clean profile history
 profile -memory on  % Bring memory info
 
-%% 1 -Opening and reading the bag
+%% 1 -> Opening and reading the bag
 
-% Function that do the first part of the code
-
-% Selecting the bag
-
+% Define the bag
 b = 'Puck-uINS_2020-07-14-14-43-29.bag';
 rosbag info 'Puck-uINS_2020-07-14-14-43-29.bag'
 
+% Read the bag to Lidar-INS format
 [bag, Lidar, INS] = read_bag(b);
+
+%% 2 -> Creating Lidar_concat & Lidar_XYZ
+
+[Lidar_concat, Lidar_XYZ] = explore_Lidar(Lidar);
+
+
+
